47b3dd4f5f81f33b15c679ee90914585012a238b
Flurin Zeberli <flurin.zeberli@gmail.com>
2021-08-16 13:06:50 +0200

    create and delete file

--- Streams/Program.cs
+++ Streams/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Streams
 {
@@ -6,7 +7,25 @@ namespace Streams
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string filename = @"ByteArray.bin";
+
+            byte[] byteArrayWrite = { 200, 201, 202, 203, 204, 205, 206, 207 };
+            byte[] byteArrayRead = new byte[byteArrayWrite.Length];
+
+            FileStream fs = new FileStream(filename, FileMode.Create);
+
+            fs.Write(byteArrayWrite, 0, byteArrayWrite.Length);
+
+            fs.Position = 0;
+            fs.Read(byteArrayRead, 0, byteArrayRead.Length);
+
+            for (int count = 0; count < byteArrayRead.Length; count++)
+            {
+                Console.Write(byteArrayRead[count] + ", ");
+            }
+
+            fs.Close();
+            File.Delete(filename);
         }
     }
 }
