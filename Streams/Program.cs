bd961a0b74dfbb07a841228a347a11016a7a4f3e
Flurin Zeberli <flurin.zeberli@gmail.com>
2021-08-16 11:54:45 +0200

    initial commit

---
+++ Streams/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
