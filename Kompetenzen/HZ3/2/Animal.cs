8139ca8396193b5b343f9d90e113ae685e086d44
Forever-a-noob <flurin.zeberli@gmail.com>
2022-01-16 19:31:27 +0100

    Create HZ3_2

---
+++ Kompetenzen/HZ3/2/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kompetenzen.HZ3._2
+{
+    abstract class Animal
+    {
+        public int Size { get; set; }
+        public abstract void Grow();
+    }
+}
