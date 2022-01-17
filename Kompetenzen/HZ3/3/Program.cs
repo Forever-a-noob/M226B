1b83ac7489b67e141943ad208e6f1c8712a37ea8
flurin.zeberli@gmail.com <flurin.zeberli@gmail.com>
2022-01-17 21:16:14 +0100

    Fix HZ3_3

--- Kompetenzen/HZ3/3/Program.cs
+++ Kompetenzen/HZ3/3/Program.cs
@@ -4,17 +4,17 @@ namespace Kompetenzen.HZ3._3
 {
     class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    IDogNews objInform = new ImplementIDogNews();
-        //    objInform.handler += new EventHandler(Informed);
-        //    objInform.PublishDogNews();
-        //    Console.ReadLine();
-        //}
+        static void Main(string[] args)
+        {
+            IDogNews objInform = new ImplementIDogNews();
+            objInform.handler += new EventHandler(Informed);
+            objInform.PublishDogNews();
+            Console.ReadLine();
+        }
 
-        //private static void Informed(object sender, EventArgs e)
-        //{
-        //    Console.WriteLine("Informed");
-        //}
+        private static void Informed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Informed");
+        }
     }
 }
